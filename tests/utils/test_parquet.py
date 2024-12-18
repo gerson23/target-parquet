9fa0148aa7aab13097b5e80767a6ba5752c31e2e
Joao Amaral <7281460+joaopamaral@users.noreply.github.com>
2024-12-18 12:16:07 -0300

    Update test_parquet.py

--- tests/utils/test_parquet.py
+++ tests/utils/test_parquet.py
@@ -172,5 +172,3 @@ def test_get_pyarrow_table_size(sample_data, sample_schema):
     # Check if the result is a non-negative float
     assert isinstance(size_in_mb, float)
     assert pytest.approx(size_in_mb, 0.1) == 7.15
-
-
